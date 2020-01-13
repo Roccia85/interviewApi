e90f0e566416926e473adde82c6a17d7e1c85586
Marco <carrara85@gmail.com>
2020-01-13 15:09:25 +0100

    feat(firstCommit)

---
+++ chartCrypto.js
@@ -0,0 +1,8 @@
+// alphavantage apiKey J06QACM0IZY7CCHY 
+// https://www.alphavantage.co/documentation (closing daily US con USD market)
+const moment = require("moment") //https://momentjs.com/docs/
+const exampleData = require("./exampleDataCrypto.json")
+module.exports.default = function (req, res, next) {
+    var rp = require('request-promise'); //https://www.npmjs.com/package/request-promise   
+}
+
